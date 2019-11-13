6f4e54f43b9e0b23a320bb891dda1dae0a806e7d
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2019-11-13 00:32:11 +0000

    added dummy project

--- app/public/wp-content/themes/mqumairiGreen/index.php
+++ app/public/wp-content/themes/mqumairiGreen/index.php
@@ -7,64 +7,164 @@ get_header();
 
 <div id="mainContent">
 
-<!-- ABOUT CARD -->
-<div id="aboutCard">
+  <!-- ABOUT CARD -->
+  <div id="aboutCard">
 
-  <div class="cardTitle">
-    &lt;ABOUT&gt;
-  </div>
+    <div class="cardTitle">
+      &lt;ABOUT&gt;
+    </div>
 
-  <div class="cardText">
-    <p>
-      Student at City University of London, pursuing a degree in Computer Science, and on track to graduating in the First-Class with Honours. Currently seeking a summer internship in a Software Engineering role, to further programming knowledge.
-    </p>
-  </div>
-
-</div>
-
-<!-- CODING CARD -->
-<div id="codingCard">
-
-  <div class="cardTitle">
-    &lt;CODING SKILLS&gt;
-  </div>
-
-  <div class="cardTextColumn">
-
-    <div class="columnTextLeft">
+    <div class="cardText">
       <p>
-        +Java <br />
-        +C++ <br />
-        +Swift <br />
-        +SQL <br />
-        +Processing <br / />
+        Student at City University of London, pursuing a degree in Computer Science, and on track to graduating in the First-Class with Honours. Currently seeking a summer internship in a Software Engineering role, to further programming knowledge.
       </p>
     </div>
 
-    <div class="columnTextRight">
-      <p>
-        +HTML <br />
-        +CSS <br />
-        +JavaScript <br />
-        +PHP <br />
-      </p>
+  </div>
+
+  <!-- CODING CARD -->
+  <div id="codingCard">
+
+    <div class="cardTitle">
+      &lt;CODING SKILLS&gt;
+    </div>
+
+    <div class="cardTextColumn">
+
+      <div class="columnTextLeft">
+        <p>
+          +Java <br />
+          +C++ <br />
+          +Swift <br />
+          +SQL <br />
+          +Processing <br / />
+        </p>
+      </div>
+
+      <div class="columnTextRight">
+        <p>
+          +HTML <br />
+          +CSS <br />
+          +JavaScript <br />
+          +PHP <br />
+        </p>
+
+      </div>
 
     </div>
 
   </div>
 
-</div>
+  <!-- DESIGN CARD -->
 
-<!-- DESIGN CARD -->
+  <div id="designCard">
 
-<div id="designCard">
+    <div class="cardTitle">
+      &lt;DESIGN SKILLS&gt;
+    </div>
 
-  <div class="cardTitle">
-    &lt;DESIGN SKILLS&gt;
+    <div class="cardText">
+      <div class="columnSingle">
+        <p>
+          +Affinity Designer <br />
+          +Adobe Photoshop <br />
+          +UML Modeling <br />
+          +ERD Modeling <br />
+          +Office Suite <br / />
+        </p>
+      </div>
+    </div>
+
   </div>
 
+
+<!-- PROGRAMMING CARD -->
+
+<div id="programmingProjects">
+
+  <div class="cardTitle">
+    &lt;PROGRAMMING PROJECTS&gt;
+  </div>
+
+  <div class="projectPost">
+    <div class="projectImage"><img src="https://mqumairi.com/wp-content/uploads/2019/11/budgetBuddies.png" /></div>
+
+    <div class="projectText">
+      <div class="projectTitle">BUDGET BUDDIES</div>
+      <div class="projectDescription">A template for Budget Buddies, designed during the 48in48 hacakthon in London.</div>
+      <div class="projectShare">
+        <div class="readMore">READ MORE</div>
+        <div class="view"></div>
+        <div class="git"></div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="projectPost">
+    <div class="projectImage"><img src="https://mqumairi.com/wp-content/uploads/2019/11/budgetBuddies.png" /></div>
+
+    <div class="projectText">
+      <div class="projectTitle">BUDGET BUDDIES</div>
+      <div class="projectDescription">A template for Budget Buddies, designed during the 48in48 hacakthon in London.</div>
+      <div class="projectShare">
+        <div class="readMore">READ MORE</div>
+        <div class="view"></div>
+        <div class="git"></div>
+      </div>
+    </div>
+
+  </div>
+
+
 </div>
 
+
+<!-- WEB CARD -->
+
+<div id="webProjects">
+
+  <div class="cardTitle">
+    &lt;WEB PROJECTS&gt;
+  </div>
+
+  <div class="projectPost">
+    <div class="projectImage"><img src="https://mqumairi.com/wp-content/uploads/2019/11/budgetBuddies.png" /></div>
+
+    <div class="projectText">
+      <div class="projectTitle">BUDGET BUDDIES</div>
+      <div class="projectDescription">A template for Budget Buddies, designed during the 48in48 hacakthon in London.</div>
+      <div class="projectShare">
+        <div class="readMore">READ MORE</div>
+        <div class="view"></div>
+        <div class="git"></div>
+      </div>
+    </div>
+
+  </div>
+
+
+  <div class="projectPost">
+    <div class="projectImage"><img src="https://mqumairi.com/wp-content/uploads/2019/11/budgetBuddies.png" /></div>
+
+    <div class="projectText">
+      <div class="projectTitle">BUDGET BUDDIES</div>
+      <div class="projectDescription">A template for Budget Buddies, designed during the 48in48 hacakthon in London.</div>
+      <div class="projectShare">
+        <div class="readMore">READ MORE</div>
+        <div class="view"></div>
+        <div class="git"></div>
+      </div>
+    </div>
+
+  </div>
+
+
+</div>
+
+
+
+<!-- END OF MAIN CONTENT -->
 </div>
 
 
